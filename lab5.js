b2a30475a975e0b283aeaa2580dec3c0250d0235
IvanLuchkin <id.luchkin@gmail.com>
2019-11-01 12:28:36 +0200

    server

--- lab5.js
+++ lab5.js
@@ -1,3 +1,20 @@
+const http = require('http')
+const hostname = '127.0.0.1'
+const port = process.env.PORT || 3000
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Hi there!\n')
+})
+/*server.get("/", function(req, res) {
+  res.send
+})
+*/
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
+
 function total () {
   document.getElementById('result').value = 'The total price of the courses: $' + (coursePrice('Java') + coursePrice('Cpp') + coursePrice('Cs') + coursePrice('Js') + coursePrice('HtmlCss'))
 }
